51b3151ed0dd2542212202999bdc9edf4e79547c
Jasur <ibraimov0409@mail.ru>
2023-04-05 15:50:20 +0600

    cords

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const PORT = 8000 
 const db = require("./firebase.js")
-
+app.use(cors())
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
