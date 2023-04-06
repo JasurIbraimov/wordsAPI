9078a348b1768bf21c78d9b894be42b1ef2d51c5
Jasur <ibraimov0409@mail.ru>
2023-04-06 14:55:03 +0600

    Added words pages and created styles for the project

--- index.js
+++ index.js
@@ -7,31 +7,70 @@ app.use(cors())
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.set('view engine', 'ejs');
 
-app.get("/word", (req, res) => {
-    return res.sendFile(__dirname + "/public/index.html")
-})
-
-app.post("/word", async (req, res) => {
-    const { wordEng, wordRu, category } = req.body;
-    await db.collection("words").add({
-        word: {ru: wordRu, eng: wordEng},
-        category: category
-    })
-
-    return res.redirect("/words/word")
-})
-
-app.get("/:lang/:category/", async (req, res) => {
-    const { lang, category } = req.params;
+async function fetchWordsByLangAndCategory(lang, category) {
     const snapShot = await db.collection("words").get()
     let data = []
     snapShot.forEach(doc => {
         data.push(doc.data())
     })
     data = data.filter(q => q.category == category).map(q => q.word[lang])
+    return data
+}
+
+app.get("/word", (req, res) => {
+    return res.render(__dirname + "/public/index", {message: ""})
+})
+
+app.post("/word", async (req, res) => {
+    const { wordEng, wordRu, category } = req.body;
+    const snapShot = await db.collection("words").get()
+    if (wordEng == "" || wordRu == "") {
+        return res.status(404).send({"error": "The word has no english or russian version"})
+    }
+    let data = []
+    snapShot.forEach(doc => {
+        data.push(doc.data())
+    })
+
+    if(data.findIndex((d) => d.word.ru == wordRu || d.word.eng == wordEng) != -1) {
+        return res.status(404).send({ "error": "This word is already exist in database!" })
+    }
+
+    await db.collection("words").add({
+        word: {ru: wordRu, eng: wordEng},
+        category: category
+    })
+
+    return res.render(__dirname + "/public/index", {message: "Word Added Successfully!"})
+})
+
+app.get("/word/delete/:id", async (req, res) => {
+    const {id} = req.params;
+    const doc = await db.collection("words").doc(id).get()
+    await db.collection("words").doc(id).delete()
+    return res.status(200).send({message: "The word " + doc.data().word.ru + " - " +doc.data().word.eng + " successfully deleted!"})
+})
+
+app.get("/words/:lang/:category", async (req, res) => {
+    const { lang, category } = req.params;
+    const snapShot = await db.collection("words").get()
+    let data = []
+    snapShot.forEach(doc => {
+        data.push({...doc.data(), id: doc.id})
+    })
+    data = data.filter(q => q.category == category).map(q => ({word: q.word[lang], id: q.id}))
+    return res.render(__dirname + "/public/words", {words: data})
+})  
+
+app.get("/:lang/:category/", async (req, res) => {
+    const { lang, category } = req.params;
+    let data = await fetchWordsByLangAndCategory(lang, category)
     return res.send(data)
 })
+
+
 
 app.listen(PORT, () => {
     console.log("SERVER IS RUNNING")
